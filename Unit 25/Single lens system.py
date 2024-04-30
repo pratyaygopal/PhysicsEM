e50f7bb1e156dc11b7df26a9cee307d6e9daaae5
RFire5 <66963492+RFire5@users.noreply.github.com>
2024-04-30 01:54:32 -0500

    Update Single lens system.py

--- Unit 25/Single lens system.py
+++ Unit 25/Single lens system.py
@@ -1,17 +1,17 @@
 import numpy as np
 
-u = 133
-v = 74.8
-y1 = 6.57
-n = 1.49
+u = 72.8
+v = 37.5
+y1 = 3.44
+n = 1.68
 
-x1new = -28.7
+x1new = -13.9
 
 flens = 1/((1/v) + (1/u))
 print(flens) #Q1
 
-y2 = (u/v)y1
-print(y2 -1) #Q2
+y2 = (u/v)*y1
+print(y2*-1) #Q2
 
 rlens = (n - 1) * flens
 print(rlens) #Q3
